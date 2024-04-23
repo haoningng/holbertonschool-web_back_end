56fffb965d069a94b8c51c169e30473f5a0fa9a6
haoningng <nick.nghn@gmail.com>
2024-04-23 20:28:57 +1000

    feat: add task 2

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""This module defines an asynchronous coroutine"""
+import asyncio
+import time
+from typing import List
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    """
+    This coroutine measures the total execution time for
+    wait_n(n, max_delay) and returns total_time/n
+
+    Arguments:
+    n(int): number of times wait_random is called
+    max_delay(int): max delay time for wait_random
+    """
+    s: float = time.perf_counter()
+    results: List[float] = await wait_n(n, max_delay)
+    total_time: float = time.perf_counter() - s
+    return total_time/n
