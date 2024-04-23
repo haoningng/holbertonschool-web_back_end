d4bae5e07e1c922bc36021767968353625bb04c9
haoningng <nick.nghn@gmail.com>
2024-04-23 13:42:09 +1000

    feat: add task 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""This module defines the to_str function"""
+
+
+def to_str(n: float) -> str:
+    """This function takes a float n and returns the str representation
+
+    Argument:
+    n(float): float argument
+    """
+    return str(n)
