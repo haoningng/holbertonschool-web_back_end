3fa12e893bbae48f2e8d4ddefc6f9027295f0ad9
haoningng <nick.nghn@gmail.com>
2024-04-23 13:31:38 +1000

    feat: add task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""This module defines the concat function"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """This function returns a concatenated string by taking 2 str arguments
+
+    Arguments:
+    str1(str): First string
+    str2(str): Second string
+    """
+    return (str1 + str2)
