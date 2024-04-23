5d7152cdda8cc7244c0bf52556bc0ccd24f9e6a3
haoningng <nick.nghn@gmail.com>
2024-04-23 13:35:01 +1000

    feat: add task 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""This module defines the floor function"""
+
+
+def floor(n: float) -> int:
+    """This function takes a float argument as returns the floor of the float
+
+    Argument:
+    n(float): float argument
+    """
+    return int(n)
