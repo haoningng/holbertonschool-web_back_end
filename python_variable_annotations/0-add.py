8fe0f483d65bf163695edd8254c516da905f00ed
haoningng <nick.nghn@gmail.com>
2024-04-23 13:27:24 +1000

    feat: add task 0

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module defines an add function"""
+
+
+def add(a: float, b: float) -> float:
+    """This is function return the sum of two floats as float"""
+    return a + b
