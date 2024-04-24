e9d8402e910bbe83f41817b77cb0a90c57bb954f
haoningng <nick.nghn@gmail.com>
2024-04-24 18:36:21 +1000

    fix(task0): use Generator as type hint instead of AsyncGenerator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """This module defines an asynchronous coroutine"""
 import asyncio
 import random
-from typing import List, AsyncGenerator
+from typing import List, Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """This coroutine will loop 10 times,
     each time asynchronously wait 1 second,
     then yield a random number between 0 and 10
