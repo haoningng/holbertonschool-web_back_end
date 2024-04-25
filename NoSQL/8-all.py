536629145f09cf1c18314f2e70473d47e3575592
haoningng <nick.nghn@gmail.com>
2024-04-25 19:30:55 +1000

    fix: return list

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 """This module defines a function"""
 import pymongo
-from typing import Any
+from typing import List, Any
 
 
 def list_all(
              mongo_collection: pymongo.collection.Collection
-            ) -> pymongo.cursor.Cursor[Any]:
+            ) -> List[Any]:
     """This function lists all documents in a collection:"""
-    return mongo_collection.find()
+    doc_cursor: pymongo.cursor.Cursor[Any] = mongo_collection.find()
+    doc_list: List[Any] = []
+    for doc in doc_cursor:
+        doc_list.append(doc)
+    return doc_list
