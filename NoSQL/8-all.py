abb45de3812a428ae702ca2a74c63cbba82365ee
haoningng <nick.nghn@gmail.com>
2024-04-25 19:26:07 +1000

    fix(task8): add docstrings, add type hints

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 """This module defines a function"""
+import pymongo
+from typing import Any
 
 
-def list_all(mongo_collection):
-    return list(mongo_collection.find())
+def list_all(
+             mongo_collection: pymongo.collection.Collection
+            ) -> pymongo.cursor.Cursor[Any]:
+    """This function lists all documents in a collection:"""
+    return mongo_collection.find()
