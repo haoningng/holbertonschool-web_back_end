4b9600d001702b628fef559967396b452feab812
haoningng <nick.nghn@gmail.com>
2024-04-26 19:07:15 +1000

    fix(task2): use asyncio.run instead

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -15,9 +15,7 @@ async def measure_time(n: int, max_delay: int) -> float:
     n(int): number of times wait_random is called
     max_delay(int): max delay time for wait_random
     """
-    start_time = time.time()
+    s: float = time.perf_counter()
     asyncio.run(wait_n(n, max_delay))
-    end_time = time.time()
-    total_time = end_time - start_time
-    average_time = total_time / n
-    return average_time
+    total_time: float = time.perf_counter() - s
+    return total_time / n
