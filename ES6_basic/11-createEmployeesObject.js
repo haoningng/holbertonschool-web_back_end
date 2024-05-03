10fb69abe79b49a6c914ff14d752d108caa7d62a
haoningng <nick.nghn@gmail.com>
2024-05-03 20:10:35 +1000

    feat(task11): use const

--- ES6_basic/11-createEmployeesObject.js
+++ ES6_basic/11-createEmployeesObject.js
@@ -1,5 +1,7 @@
 export default function createEmployeesObject(departmentName, employees) {
-  return {
+  const employeesObject = {
     departmentName: employees,
   };
+
+  return employeesObject;
 }
