1edcc9bea17aab16e7f3e367bd7fdbbf93a9b8cd
haoningng <nick.nghn@gmail.com>
2024-05-04 14:10:10 +1000

    feat(task 8): add task and test file

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,37 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    if (typeof size !== 'number') {
+      throw new TypeError('Size must be a number');
+    }
+    if (typeof location !== 'string') {
+      throw new TypeError('Location must be a string');
+    }
+    this._size = size;
+    this._location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set size(newSize) {
+    this._size = newSize;
+  }
+
+  set location(newLocation) {
+    this._location = newLocation;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this._size;
+    } if (hint === 'string') {
+      return this._location;
+    }
+    return this;
+  }
+}
