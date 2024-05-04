8cec3b5f139b52224610b4f11b46977257cc0edb
haoningng <nick.nghn@gmail.com>
2024-05-04 14:00:18 +1000

    feat(task7): add task and test file

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,32 @@
+export default class Airport {
+  constructor(name, code) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set name(newName) {
+    this._name = newName;
+  }
+
+  set code(newCode) {
+    this._code = newCode;
+  }
+
+  get [Symbol.toStringTag]() {
+    return `${this._code}`;
+  }
+}
