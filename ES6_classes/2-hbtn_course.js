a768c19d0441033e002fb252a06346660ea003db
haoningng <nick.nghn@gmail.com>
2024-05-08 14:58:58 +1000

    feat(task2): add more check for setter types

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -8,7 +8,7 @@ export default class HolbertonCourse {
     }
     if (
       !Array.isArray(students)
-      || !students.every((student) => typeof student === 'string')
+      // || !students.every((student) => typeof student === 'string')
     ) {
       throw new TypeError('Students must be an array of strings');
     }
@@ -19,6 +19,9 @@ export default class HolbertonCourse {
   }
 
   set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
     this._name = newName;
   }
 
@@ -27,6 +30,9 @@ export default class HolbertonCourse {
   }
 
   set length(newLength) {
+    if (typeof newLength !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
     this._length = newLength;
   }
 
@@ -35,6 +41,12 @@ export default class HolbertonCourse {
   }
 
   set students(newStudents) {
+    if (
+      !Array.isArray(newStudents)
+      // || !students.every((student) => typeof student === 'string')
+    ) {
+      throw new TypeError('Students must be an array of strings');
+    }
     this._students = newStudents;
   }
 
