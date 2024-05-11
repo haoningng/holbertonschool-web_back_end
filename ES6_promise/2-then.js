bc411a97a2c01283ff33bc6d51be58b5a8cbfaf1
haoningng <nick.nghn@gmail.com>
2024-05-11 20:40:51 +1000

    fix(task2): add finally

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,12 +1,8 @@
 export default async function handleResponseFromAPI(promise) {
-  promise.then(() => {
+  promise.then(() => ({
+    status: 200,
+    body: 'success',
+  })).catch(() => new Error()).finally(() => {
     console.log('Got a response from the API');
-    return {
-      status: 200,
-      body: 'Success',
-    };
-  }).catch(() => {
-    const err = new Error();
-    return err;
   });
 }
