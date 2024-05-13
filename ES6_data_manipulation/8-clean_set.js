ebdd95124a8e659de0519f24b01a7facb718269c
haoningng <nick.nghn@gmail.com>
2024-05-14 09:59:01 +1000

    feat(task8): add task and test file

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,8 @@
+export default function cleanSet(set, startString) {
+  if (startString.length === 0) {
+    return '';
+  }
+  const newArray = Array.from(set).filter((each) => each.includes(startString));
+  const result = newArray.map((each) => each.slice(startString.length)).join('-');
+  return result;
+}
