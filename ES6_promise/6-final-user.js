ab0e9aba9f1fb6f97b66c6756754b0e3d718733b
haoningng <nick.nghn@gmail.com>
2024-05-13 19:19:04 +1000

    fix(task6): use reason.message instead of reason

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -5,11 +5,10 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((values) => {
       const newList = [];
-
       for (const each of values) {
         newList.push({
           status: each.status,
-          value: each.status === 'fulfilled' ? each.value : each.reason,
+          value: each.status === 'fulfilled' ? each.value : each.reason.message,
         });
       }
       return newList;
