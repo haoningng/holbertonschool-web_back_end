2a8c3ae21c9b21d3e006c8e12f0ee56e2206988b
haoningng <nick.nghn@gmail.com>
2024-05-13 19:11:14 +1000

    feat(task6): add task and test file

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,17 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((values) => {
+      const newList = [];
+
+      for (const each of values) {
+        newList.push({
+          status: each.status,
+          value: each.status === 'fulfilled' ? each.value : each.reason,
+        });
+      }
+      return newList;
+    });
+}
