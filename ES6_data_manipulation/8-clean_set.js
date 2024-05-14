57f76a51ac91aba164a12eba5260b8fa09db0416
haoningng <nick.nghn@gmail.com>
2024-05-14 10:33:05 +1000

    feat(task8): add check for type string

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -2,6 +2,9 @@ export default function cleanSet(set, startString) {
   if (startString.length === 0) {
     return '';
   }
+  if (typeof startString !== 'string') {
+    return '';
+  }
   const newArray = Array.from(set).filter((each) => each.includes(startString));
   const result = newArray.map((each) => each.slice(startString.length)).join('-');
   return result;
